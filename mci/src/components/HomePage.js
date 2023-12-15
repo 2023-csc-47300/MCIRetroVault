317b25b784e37352474bce454630c8fba7e38e9b
CS9490 <cristianstatescu.cs9490@gmail.com>
2023-12-15 13:16:43 -0500

    added buttons at the top of each page to allow easy transitions to different pages. also made them dynamically change if user is logged in or not logged in

--- mci/src/components/HomePage.js
+++ mci/src/components/HomePage.js
@@ -1,20 +1,49 @@
 import React from 'react';
-import { Link } from 'react-router-dom';  
+import { Link, useNavigate} from 'react-router-dom';  
 import MCIRetroVaultImage from '../img/MCIRetro_Vault.png';
+import AuthService from '../services/AuthService'; // Import your AuthService
 
 function HomePage() {
+  const navigate = useNavigate();
+  const safeParse = (data) => {
+      try {
+          return JSON.parse(data);
+      } catch (e) {
+          return null;
+      }
+  };
+  const user = safeParse(localStorage.getItem('user'));
+
+  const handleLogout = () => {
+    AuthService.logout(); // Clear the user session
+    navigate('/'); // Redirect to home page or login page
+  };
+
   return (
     <>
       <header>
         <div className="header-container">
           <div className="header-left">
             <img src={MCIRetroVaultImage} alt="MCIRetro Vault" style={{ width: '100px', height: 'auto' }} />
-            <h1>Welcome to MCIRetroVault Website</h1>
+            <h1>MCIRetroVault</h1>
           </div>
           <div className="header-right">
-            <Link to="/signin" className="header-button">Sign In</Link>
-            <Link to="/signup" className="header-button">Sign Up</Link>
-            <Link to="/search" className="header-button">Search</Link>
+            {user ? (
+              <>
+                {/* User is logged in */}
+                <Link to="/search" className="header-button">Search</Link>
+                <button onClick={handleLogout} className="header-button">Logout</button>
+                
+                {/* Add more links as needed */}
+              </>
+            ) : (
+              <>
+                {/* User is not logged in */}
+                <Link to="/signin" className="header-button">Sign In</Link>
+                <Link to="/signup" className="header-button">Sign Up</Link>
+                <Link to="/search" className="header-button">Search</Link>
+              </>
+            )}
           </div>
         </div>
       </header>
--- mci/src/components/Search.js
+++ mci/src/components/Search.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';  
+import { Link, useNavigate } from 'react-router-dom';  
 import MCIRetroVaultImage from '../img/MCIRetro_Vault.png';
-import '../index.css';
+import AuthService from '../services/AuthService'; // import AuthService
+
 
 import { useState, useEffect } from 'react';
 import $ from 'jquery';
 
-function HomePage() {
+function Search() {
+    const navigate = useNavigate();
+    const safeParse = (data) => {
+        try {
+            return JSON.parse(data);
+        } catch (e) {
+            return null;
+        }
+    };
+    const user = safeParse(localStorage.getItem('user'));
+
+    const handleLogout = () => {
+        AuthService.logout();
+        navigate('/search');
+    };
 
     const [gameName, setGameName] = useState('');
 
@@ -98,19 +113,28 @@ function HomePage() {
 
     return (
         <>
-        <header>
-            <div className="header-container">
-            <div className="header-left">
-                <img src={MCIRetroVaultImage} alt="MCIRetro Vault" style={{ width: '100px', height: 'auto' }} />
-                <h1>Welcome to MCIRetroVault Website</h1>
-            </div>
-            <div className="header-right">
-                <Link to="/signin" className="header-button">Sign In</Link>
-                <Link to="/signup" className="header-button">Sign Up</Link>
-                <Link to="/search" className="header-button">Search</Link>
-            </div>
-            </div>
-        </header>
+         <header>
+                <div className="header-container">
+                    <div className="header-left">
+                        <img src={MCIRetroVaultImage} alt="MCIRetro Vault" style={{ width: '100px', height: 'auto' }} />
+                        <h1>MCIRetroVault</h1>
+                    </div>
+                    <div className="header-right">
+                        <Link to="/" className="header-button">Home</Link>
+                        {user ? (
+                            <>
+                                <button onClick={handleLogout} className="header-button">Logout</button>
+                                {/* Add more user-specific links or information here */}
+                            </>
+                        ) : (
+                            <>
+                                <Link to="/signin" className="header-button">Sign In</Link>
+                                <Link to="/signup" className="header-button">Sign Up</Link>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </header>
 
         <main>
             <center>
@@ -183,4 +207,4 @@ function HomePage() {
     );
 }
 
-export default HomePage;
+export default Search;
