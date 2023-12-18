e951da3795af1ed0f663ac3a73d26b28d43e71e9
mluna1024 <mluna006@citymail.cuny.edu>
2023-12-18 03:13:05 -0500

    Finished css and links of Platforms page

--- mci/src/components/Search.js
+++ mci/src/components/Search.js
@@ -131,6 +131,7 @@ function Search() {
                                 <Link to="/signup" className="header-button">Sign Up</Link>
                             </>
                         )}
+                        <Link to="/platforms" className="header-button">Platforms</Link>
                     </div>
                 </div>
             </header>
