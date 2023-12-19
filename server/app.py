0256654126cf4072bc2f4d890e43df74c4af005f
CS9490 <cristianstatescu.cs9490@gmail.com>
2023-12-19 02:32:49 -0500

    flask deployement

--- server/app.py
+++ server/app.py
@@ -265,6 +265,7 @@ def get_user_email():
 # Other routes...
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
     with app.app_context():
         db.create_all()  # Create database tables
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=port)
